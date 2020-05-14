131109f5eb56bd48a955fddc651c9d7a56bd588d
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-14 18:37:06 -0300

    Projeto ParImpar de Array

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            int pares = 0;
+            int impares = 0;
+            int[] listanumeros = new int[10];
+
+            Console.WriteLine("Digite uma sequencia de números de 1 a 10");
+            Console.ReadLine();
+
+            Console.Clear();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite o " + (i + 1) + "º numero:");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                listanumeros[i] = n1;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (listanumeros[i] % 2 == 0)
+                    pares ++;
+                else
+                    impares ++;
+            }
+
+            Console.Clear();
+            Console.WriteLine("O total de números pares: " + pares);
+            Console.WriteLine("O total de número impares: " + impares);
+
+            Console.ReadLine();
+
+        }
+    }
+}
